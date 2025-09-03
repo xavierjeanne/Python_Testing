218d4a8affea608cc8f8ae39da9621b70ceda457
xjeanne <x.jeanne@102-103.com>
2025-09-03 21:37:30 +0200

    Fix email not found and add message

---
+++ tests/test_server.py
@@ -0,0 +1,19 @@
+import pytest
+from server import app
+
+def test_showSummary_email_not_found(client):
+    response = client.post('/showSummary', data={'email': 'notfound@email.com'})
+    html_content = response.data.decode('utf-8')
+    assert "Cet email n&#39;existe pas. Veuillez vérifier votre saisie." in html_content
+    assert response.status_code == 200
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_showSummary_email_found(client):
+    response = client.post('/showSummary', data={'email': 'john@simplylift.co'})
+    assert "welcome" in response.data.decode('utf-8').lower()
+    assert response.status_code == 200
